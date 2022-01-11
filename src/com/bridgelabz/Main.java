4fe5d7862be5ddc7b9f1ecfce8c2427086732a92
Amol Ghotale <amol9.ag@gmail.com>
2022-01-11 20:15:03 +0530

    ADDED: MODIFIED: After 20 days

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,19 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
         // Welcome to the game of Gambling.
-        double stake = 100;
-        int result;
-        while (stake > 0) {
-        result = (int)Math.floor((Math.random() * 10) % 2);
-        //System.out.println(a);
-        if (result == 0) {
-            System.out.println("Win");
-            stake = stake + 1;
-        }    else {
-            System.out.println("Lose");
-            stake = stake -1;
+
+        int days = 0, win = 0, lose = 0;
+        while (days <= 20) {
+            int stake = 100;
+            int result, turn = 0;
+            while (stake > 0) {
+                result = (int) Math.floor((Math.random() * 10) % 2);
+                System.out.println(result);
+                if (result == 0) {
+                    //     System.out.println("Win");
+                    stake = stake + 1;
+                    win++;
+                } else {
+                    System.out.println("Lose");
+                    stake = stake - 1;
+                    lose--;
+                }
+                if (stake == 150 || stake == 50) {
+                    stake = 0;
+
+                }
+                turn++;
+                System.out.println("Turn No" + turn);
+                System.out.println("Amount left " +stake);
+            }
+            days++;
         }
-            stake = stake - (0.5 * stake);
+        int outcome = win + lose;
+        System.out.println("Total Profits or loss after 20 days " + outcome);
 
     }
 }
