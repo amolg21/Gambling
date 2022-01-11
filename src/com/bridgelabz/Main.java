6db153ff4a3c79ff5be3d36112efaa4847ca2352
Amol Ghotale <amol9.ag@gmail.com>
2022-01-11 19:20:57 +0530

    ADDED: 50% stake

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -2,20 +2,23 @@ package com.bridgelabz;
 
 public class Main {
 
-    final int stake = 100;
     final int bet = 1;
 
     public static void main(String[] args) {
         // Welcome to the game of Gambling.
+        double stake = 100;
         int result;
+        while (stake > 0) {
         result = (int)Math.floor((Math.random() * 10) % 2);
         //System.out.println(a);
         if (result == 0) {
             System.out.println("Win");
+            stake = stake + 1;
         }    else {
             System.out.println("Lose");
+            stake = stake -1;
         }
-
+            stake = stake - (0.5 * stake);
 
     }
 }
