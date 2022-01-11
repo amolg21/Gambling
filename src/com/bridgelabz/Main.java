0bb46f9b69bb0cbefa073ffb6d6cfd4bfb375fee
Amol Ghotale <amol9.ag@gmail.com>
2022-01-11 19:06:15 +0530

    ADDED: Win or Lose

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -7,6 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         // Welcome to the game of Gambling.
+        int result;
+        result = (int)Math.floor((Math.random() * 10) % 2);
+        //System.out.println(a);
+        if (result == 0) {
+            System.out.println("Win");
+        }    else {
+            System.out.println("Lose");
+        }
 
 
     }
