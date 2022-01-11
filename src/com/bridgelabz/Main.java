87fe268ee33f7b33fef24835b72ef81e91dee289
Amol Ghotale <amol9.ag@gmail.com>
2022-01-11 18:52:00 +0530

    ADDED: Stake and bet

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -2,7 +2,12 @@ package com.bridgelabz;
 
 public class Main {
 
+    final int stake = 100;
+    final int bet = 1;
+
     public static void main(String[] args) {
-	// write your code here
+        // Welcome to the game of Gambling.
+
+
     }
 }
